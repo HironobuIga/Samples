8df45ec6149aa42930d98e3b255e1bcf47bbf363
hironobu-iga <hironobu.iga.developer@gmail.com>
2017-10-24 23:54:44 +0900

    Extensionの追加

---
+++ 01_iOS/RXSwiftSample/RXSwiftSample/BindingExtensions.swift
@@ -0,0 +1,55 @@
+//
+//  BindingExtensions.swift
+//  RXSwiftSample
+//
+//  Created by 伊賀裕展 on 2017/10/24.
+//  Copyright © 2017年 伊賀裕展. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+extension ValidationResult: CustomStringConvertible {
+    var description: String {
+        switch  self {
+        case let .ok(message):
+            return message
+        case .empty:
+            return ""
+        case .validating:
+            return "validating..."
+        case let .failed(message):
+            return message
+        }
+    }
+}
+
+struct ValidationColors {
+    static let okColor = UIColor(red: 138.0 / 255.0, green: 221.0 / 255.0, blue: 109.0 / 255.0, alpha: 1.0)
+    static let errorColor = UIColor.red
+}
+
+extension ValidationResult {
+    var textColor: UIColor {
+        switch self {
+        case .ok:
+            return ValidationColors.okColor
+        case .empty:
+            retrn .black
+        case .validating:
+            return .black
+        case .failed:
+            return ValidationColors.errorColor
+        }
+    }
+}
+
+extension Reactive where Base: UILabel {
+    var validationResult: Binder<ValidationResult> {
+        return Binder(base) { label, result in
+            label.textColor = result.textColor
+            label.text = result.description
+        }
+    }
+}
