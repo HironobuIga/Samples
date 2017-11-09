b2744b55618188563f34a006cdb3adab09b49a60
hironobu-iga <hironobu.iga.developer@gmail.com>
2017-11-10 01:26:04 +0900

    viewControllerの追加

--- 01_iOS/20171031/ViewAnimatorSample/ViewAnimatorSample/ViewController.swift
+++ 01_iOS/20171031/ViewAnimatorSample/ViewAnimatorSample/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import ViewAnimator
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var targetView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func didTouchUpInsideDirectionButton(_ sender: Any) {
+        let fromAnimation = AnimationType.from(direction: .left, offset: 30.0)
+//        let toAnimation = AnimationType.from(direction: .left, offset: 30.0)
+        targetView.animate(animations: [fromAnimation])
     }
-
-
+    
+    @IBAction func didTouchUpInsideZoomButton(_ sender: UIButton) {
+        let biggerAnimation = AnimationType.zoom(scale: 2.0)
+        let smallerAnimation = AnimationType.zoom(scale: 0.5)
+        targetView.animateAll(animations: [biggerAnimation, smallerAnimation], interval: 1.0)
+    }
+    
+    @IBAction func didTouchUpInsideRotateButton(_ sender: UIButton) {
+        let leftRotateAnimation = AnimationType.rotate(angle: CGFloat(-0.5 * Float.pi))
+        let rightRotateAnimation = AnimationType.rotate(angle: CGFloat(0.5 * Float.pi))
+        targetView.animate(animations: [leftRotateAnimation, rightRotateAnimation],
+                           initialAlpha: 0.5,
+                           finalAlpha: 1.0,
+                           delay: 1.0,
+                           duration: 1.0,
+                           completion: nil)
+    }
+    
+    @IBAction func didTouchUpInsideCombinationButton(_ sender: UIButton) {
+    }
+    
 }
 
